afda227bdb2ef87cecdab69a0458ef613f5ea006
hariprasadVenugopal <150322392+hariprasadVenugopal@users.noreply.github.com>
2024-09-15 13:55:58 +1000

    Update config.py

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv("postgresql://sit722_part3_afoj_user:Ypry7RpbjBpsjBiUHY3Y3jmpJcFEdokT@dpg-crij5ou8ii6s73f4fi8g-a.singapore-postgres.render.com/sit722_part3_afoj")
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
