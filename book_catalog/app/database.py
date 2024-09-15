8055d89b109e93dab75a2d8029f13ad528651281
hariprasadVenugopal <150322392+hariprasadVenugopal@users.noreply.github.com>
2024-09-15 13:56:31 +1000

    Update database.py

--- book_catalog/app/database.py
+++ book_catalog/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL ="postgresql://sit722_part3_afoj_user:Ypry7RpbjBpsjBiUHY3Y3jmpJcFEdokT@dpg-crij5ou8ii6s73f4fi8g-a.singapore-postgres.render.com/sit722_part3_afoj"   #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
